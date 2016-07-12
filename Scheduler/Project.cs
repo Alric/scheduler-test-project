a96e05adde78d6268f027d11949843b3aaf71803
Emily Gorcenski <gorcee@hotmail.com>
2016-07-12 16:06:38 -0400

    Add basic class types

---
+++ Scheduler/Project.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler
+{
+    public class Project
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public CityType City { get; set; }
+
+        public Project() { }
+        public Project(DateTime startDate, DateTime endDate, CityType city) : this()
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            City = city;
+        }
+    }
+}
