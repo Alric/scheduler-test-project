5c060ffd975e1d444d3a703d40a424444f735ead
Emily Gorcenski <gorcee@hotmail.com>
2016-07-12 17:09:08 -0400

    Update Scheduler project

---
+++ Scheduler/Scheduler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Scheduler
+{
+    public static class Scheduler
+    {
+        public static List<Project> ReadInput(string filename)
+        {
+            string json;
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                json = sr.ReadToEnd();
+            }
+
+            if (!string.IsNullOrEmpty(json))
+                return JsonConvert.DeserializeObject<List<Project>>(json);
+
+            return new List<Project>();
+        }
+    }
+}
