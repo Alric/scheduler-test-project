8da19a48d34e88a1951fbe9ea01e38a7c745c868
Emily Gorcenski <gorcee@hotmail.com>
2016-07-13 21:48:31 -0400

    Add comments and clean up code

--- Scheduler/Project.cs
+++ Scheduler/Project.cs
@@ -13,16 +13,12 @@ namespace Scheduler
         public DateTime EndDate { get; set; }
         public CityType City { get; set; }
 
-        public Project() { }
-        public Project(string name, DateTime startDate,
-            DateTime endDate, CityType city) : this()
-        {
-            Name = name;
-            StartDate = startDate;
-            EndDate = endDate;
-            City = city;
-        }
-
+        /// <summary>
+        /// Build an Enumerator over the date range contained in this project
+        /// </summary>
+        /// <returns>A structure that can be enumerated over where each element
+        /// represents a date in the range
+        /// </returns>
         public IEnumerable<DateTime> EachProjectDay()
         {
             for (var day = StartDate.Date; day.Date <= EndDate.Date; day = day.AddDays(1))
